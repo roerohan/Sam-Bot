b011bfaeba539b9b5085025df16b8952069d9e91
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-09-22 16:42:11 +0530

    update: Mongo connect parameter modified

--- models/db.js
+++ models/db.js
@@ -5,6 +5,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/SamBot', {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
 }, (err) => {
     if (!err) {
         console.log('MongoDB Connection Succeeded.')
